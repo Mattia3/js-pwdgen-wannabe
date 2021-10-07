691a08ea65154a5a005b405c44caf23ef46e2704
Mattia3 <mcortinovis9@gmail.com>
2021-10-07 15:23:33 +0200

    Esercizio completato

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log("Esercizio js")
+
+const promptNome = prompt("Inserisci il tuo nome");
+const promptCognome = prompt("Inserisci il tuo cognome");
+const promptColorePreferito = prompt("Inserisci il tuo colore preferito");
+
+console.log ("Dati dell'utente",promptNome, promptCognome, promptColorePreferito)
+
+alert(promptNome + promptCognome + promptColorePreferito + 21);
+
